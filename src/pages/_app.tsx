ba72c871813af427bb91b678de133551d59731d1
thabti.sabeur <thabti.sabeur@gmail.com>
2025-04-21 19:28:03 +0000

    add analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,12 @@
-import './../globals.css';
-import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
+import './../globals.css'; // Assuming this is where your global styles are
+import type { AppProps } from 'next/app'; // Correct import
 
+// Wrap your app with the Analytics component
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <> <Component {...pageProps} /> <Analytics /></>
+    );
 }
 
 export default App;
